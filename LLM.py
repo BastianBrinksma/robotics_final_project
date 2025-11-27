8b4011ecd444e68981ee83fcd36b53729d628eab
Ootje123 <oschrama@gmail.com>
2025-11-27 17:54:02 +0800

    added some stuff

---
+++ LLM.py
@@ -0,0 +1,123 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+env_path = "./config/.env"
+load_dotenv(dotenv_path=env_path)
+
+# System instruction that can dictate how the model behaves in the output, can be customized as needed
+SYSTEM_INSTRUCTION = (
+        "You are a helpful assistant"
+    )
+
+# Max amount of tokens that the model can output, the Gemini 2.5 Models have this maximum amount
+# For other models need to check their documentation 
+MAX_OUTPUT_TOKENS = 65535
+MODEL_NAME = "gemini-2.5-flash-lite" # Other models: "gemini-2.5-pro", "gemini-2.5-flash"; Check different max output tokens: "gemini-2.0-flash" , "gemini-2.0-flash-lite" 
+
+# We disable the safety settings, as no moderation is needed in our tasks
+SAFETY_SETTINGS = [
+    types.SafetySetting(
+        category="HARM_CATEGORY_HATE_SPEECH", threshold="OFF"),
+    types.SafetySetting(
+        category="HARM_CATEGORY_DANGEROUS_CONTENT", threshold="OFF"),
+    types.SafetySetting(
+        category="HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold="OFF"),
+    types.SafetySetting(
+        category="HARM_CATEGORY_HARASSMENT", threshold="OFF")
+]
+
+#IMPORTANT: The script loads your API key from a `.env` file located in the `./config/` directory. 
+# You must create this file and add your API key like this: `GOOGLE_API_KEY='YOUR_API_KEY_HERE'`
+
+# We input the API Key to be able to use the Gemini models
+api_key = os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"] = api_key
+client = genai.Client(api_key=api_key)
+
+# We also set LangExtract to use the API key as well:
+if 'GEMINI_API_KEY' not in os.environ:
+    os.environ['GEMINI_API_KEY'] = api_key
+
+def prompt_gemini(
+        input_prompt: list,
+        schema = None,
+        temperature: float = 0.0,
+        system_instruction: str = SYSTEM_INSTRUCTION,
+        max_output_tokens: int = MAX_OUTPUT_TOKENS,
+        client: genai.Client = client,
+        model_name: str = MODEL_NAME,
+        new_config: types.GenerateContentConfig = None,
+        with_tools: bool = False,
+        with_parts: bool = False,
+        with_tokens_info: bool = False
+    ):
+        try:
+            # If we need a JSON schema we set up the following
+            if schema:
+                generate_content_config = types.GenerateContentConfig(
+                    temperature=temperature,
+                    system_instruction=system_instruction,
+                    max_output_tokens=max_output_tokens,
+                    response_modalities=["TEXT"],
+                    response_mime_type="application/json",
+                    response_schema=schema,
+                    safety_settings=SAFETY_SETTINGS
+                )
+            # If there is no need we leave it unstructured
+            else:
+                generate_content_config = types.GenerateContentConfig(
+                    temperature=temperature,
+                    system_instruction=system_instruction,
+                    max_output_tokens=max_output_tokens,
+                    response_modalities=["TEXT"],
+                    safety_settings=SAFETY_SETTINGS
+                )
+            
+            # We add a different custom configuration if we need it
+            if new_config:
+                generate_content_config = new_config
+            
+            # For some tasks we need a more specific way to add the contents when prompting the model
+            # So we need custom parts for it sometimes from the "types" objects
+            if with_parts:
+                response = client.models.generate_content(
+                    model=model_name,
+                    contents=types.Content(parts=input_prompt),
+                    config=generate_content_config,
+                )
+            # In the simplest form the contents can be expressed as a list [] of simple objects like str and Pillow images
+            else:
+                response = client.models.generate_content(
+                    model=model_name,
+                    contents=input_prompt,
+                    config=generate_content_config,
+                )
+
+            if with_tools:
+                # print(response)
+                # Include raw response when function calling
+                completion = response
+                if with_tokens_info:
+                    log = {
+                        "model": model_name,
+                        "input_tokens": response.usage_metadata.prompt_token_count,
+                        "output_tokens": response.usage_metadata.candidates_token_count,
+                    }
+                    return completion, log
+                return completion
+            else:
+                completion = response.text
+                if with_tokens_info:
+                    log = {
+                        "model": model_name,
+                        "input_tokens": response.usage_metadata.prompt_token_count,
+                        "output_tokens": response.usage_metadata.candidates_token_count,
+                    }
+                    # Return the text response and logs (if selected)
+                    return completion, log
+                return completion
+        except Exception as e:
+             print(f"Error occurred when generating response, error: {e}")
+             return None
